e17c01c310c347f8702b5b3184a8393247229f32
Rick Beyer <beyer.richard73@gmail.com>
2019-12-23 22:45:57 -0600

    Input onChange, onBlur, onFocus attributes work properly

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -2,11 +2,23 @@ import React, { useState } from 'react';
 
 function SearchBar(props) {
     const [name, setName] = useState('Enter City or Zip Code');
+    const [max, setMax] = useState(5);
+
+    // restrict length of input based on whether first character is a number or letter
+    function handleChange(e) {
+        if(parseInt(e.target.value) > -1) {
+            setMax(5);
+        }
+        else {
+            setMax(99);
+        }
+
+    }
 
     return (
         <section className="search">
             <div className="input-container">
-                <input type="text" onFocus={() => setName('')} onBlur={() => setName('Enter City or Zip Code')} placeholder={name}  className="user-input"/>
+                <input type="text" onChange={handleChange} maxLength={max} onFocus={() => setName('')} onBlur={() => setName('Enter City or Zip Code')} placeholder={name}  className="user-input" />
             </div>
         </section>
     )
