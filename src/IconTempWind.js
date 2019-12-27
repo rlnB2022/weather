e6363098276ef0d2db7637e07211e4d56ceeaab9
Rick Beyer <beyer.richard73@gmail.com>
2019-12-26 23:05:51 -0600

    Started working on IconTempWind.js

--- src/IconTempWind.js
+++ src/IconTempWind.js
@@ -2,7 +2,18 @@ import React from 'react';
 
 function IconTempWind(props) {
     return (
-        <div></div>
+        <div>
+            <img src="" alt="" />
+            <p></p>
+            <div className="wind-div">
+                <div className="wind-icon">
+
+                </div>
+                <div className="wind-speed">
+                    
+                </div>
+            </div>
+        </div>
     )
 }
 
