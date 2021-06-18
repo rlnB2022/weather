ce1067451d5334e128d9255c2fcf187f9b22a779
Rick Beyer <beyer.richard73@gmail.com>
2021-06-17 23:10:07 -0500

    Made some adjustments

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -4,9 +4,7 @@ import './appheader.css';
 function AppHeader(props) {
     return (
         <div className="app-header">
-            <div>
-                <p id='header-text-id' className={props.headerText}>Rick's Weather App</p>
-            </div>
+            <p id='header-text-id' className={props.headerText}>Rick's Weather App</p>
         </div>
     )
 }
