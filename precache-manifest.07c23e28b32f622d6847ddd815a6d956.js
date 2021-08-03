93fc4a3ab634af5da24cfa196fbc8a4b66251d2e
Rick Beyer <beyer.richard73@gmail.com>
2021-08-03 18:49:41 -0500

    Updates

---
+++ precache-manifest.07c23e28b32f622d6847ddd815a6d956.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b43a54786a136252feb08a02afadb549",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "7d348b6ec14140cfd76f",
+    "url": "/weather/static/css/main.0204424b.chunk.css"
+  },
+  {
+    "revision": "a221e4aa4ab669cee09d",
+    "url": "/weather/static/js/2.b1611297.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/weather/static/js/2.b1611297.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7d348b6ec14140cfd76f",
+    "url": "/weather/static/js/main.fb37f8c7.chunk.js"
+  },
+  {
+    "revision": "6ea7e77960c1989f63b6",
+    "url": "/weather/static/js/runtime-main.f857f3f3.js"
+  },
+  {
+    "revision": "82c41ec6befeeacaf80703d601c49d32",
+    "url": "/weather/static/media/mobile-day-clear.82c41ec6.jpg"
+  }
+]);
